2d956b2ce6327941cb212e7ae01ac296ede8c074
Eric-Philippe <ericphlpp@gmail.com>
2026-08-18 22:46:03 +0200

    12

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "com.chronos.reminder"
         minSdk = 31
         targetSdk = 36
-        versionCode = 11
+        versionCode = 12
         versionName = "1.3.0"
 
         testInstrumentationRunner = "com.chronos.reminder.HiltTestRunner"
